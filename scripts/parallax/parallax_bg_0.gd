3330eea85626c8d37e30d9b57365a82899d380c9
mhco0 <mhco@cin.ufpe.br>
2023-06-18 14:04:26 -0300

    Adding parallax background

---
+++ scripts/parallax/parallax_bg_0.gd
@@ -0,0 +1,13 @@
+extends ParallaxBackground
+
+@export
+var scroll_speed: float = 40.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	scroll_offset.x += scroll_speed * delta
