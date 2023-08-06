793423fea50bcad08aaf4436214e08defa5f8f10
mhco0 <mhco@cin.ufpe.br>
2023-08-06 12:02:52 -0300

    Adding enemy informations

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,18 +2,28 @@ class_name Enemy
 extends Area2D
 
 @export
-var speed: int = 400
+var speed: int = 20
+
+@export
+var progress_on_path: float = 0.02
 
 @export
 var damage_done: int = 1
 
-var velocity: Vector2 = Vector2(0, 0)
+@onready
+var path_follow: PathFollow2D = $Path2D/PathFollow2D
+
+@onready
+var path_mark: Marker2D = $Path2D/PathFollow2D/Marker2D
+
+var progress_ratio: float = speed * progress_on_path
 
 func _ready():
-	velocity = Vector2(-speed, 0)
+	path_follow.progress_ratio = progress_ratio
 
 func _physics_process(delta):
-	position += velocity * delta
+	#position += velocity * delta
+	self.position = path_mark.global_position
 	
 	if (position.x <= 0):
 		queue_free()
